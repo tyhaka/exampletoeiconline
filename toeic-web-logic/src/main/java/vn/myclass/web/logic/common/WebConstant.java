00c50cb535ad866ef0197edf48972e3d439fe4e4
tyhaka <trunght87@gmail.com>
2017-06-27 14:06:44 +0700

    trunght commt len

---
+++ toeic-web-logic/src/main/java/vn/myclass/web/logic/common/WebConstant.java
@@ -0,0 +1,11 @@
+package vn.myclass.web.logic.common;
+
+/**
+ * Created by trunght on 27/06/2017.
+ */
+public class WebConstant {
+
+
+
+
+}
---
+++ toeic-web/src/main/webapp/index.jsp
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Test hoang trung</title>
+</head>
+<body>
+<h1>Test project</h1>
+</body>
+</html>
